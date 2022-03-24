e6aa568d5346d178e628219535fa2b15787200c4
jianming <448640927@qq.com>
2022-03-24 15:54:44 +0800

    add cli

---
+++ consignment-service/go.mod
@@ -0,0 +1,8 @@
+module consignment
+
+go 1.16
+
+require (
+	github.com/Jimmy01010/shippy-service-consignment v0.0.0-20220221030830-e4fbb0147d54 // indirect
+	google.golang.org/grpc v1.44.0
+)
--- consignment-service/main.go
+++ consignment-service/main.go
@@ -8,7 +8,7 @@ import (
 	"sync"
 
 	// Import the generated protobuf code
-	pb "github.com/<YourUserName>/shippy-service-consignment/proto/consignment"
+	pb "github.com/Jimmy01010/shippy-service-consignment/proto/consignment"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -23,6 +23,7 @@ type repository interface {
 
 // Repository - Dummy repository, this simulates the use of a datastore
 // of some kind. We'll replace this with a real implementation later on.
+// 虚拟存储库，这模拟了某种数据存储的使用。稍后我们将用一个真正的实现来替换它。
 type Repository struct {
 	mu           sync.RWMutex
 	consignments []*pb.Consignment
@@ -41,13 +42,17 @@ func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, er
 // we defined in our protobuf definition. You can check the interface
 // in the generated code itself for the exact method signatures etc
 // to give you a better idea.
+// 服务应该实现所有的方法来满足我们在protobuf定义中定义的服务。
+// 你可以在生成的代码中检查这个接口，看看是否有确切的方法签名等等，这样你会有更好的想法。
 type service struct {
+	pb.UnimplementedShippingServiceServer
 	repo repository
 }
 
 // CreateConsignment - we created just one method on our service,
 // which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server.
+// 我们在服务上只创建了一个方法，这是一个create方法，它接受一个上下文和一个请求作为参数，这些都是由gRPC服务器处理的。
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
 
 	// Save our consignment
@@ -62,7 +67,6 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 }
 
 func main() {
-
 	repo := &Repository{}
 
 	// Set-up our gRPC server.
@@ -75,7 +79,8 @@ func main() {
 	// Register our service with the gRPC server, this will tie our
 	// implementation into the auto-generated interface code for our
 	// protobuf definition.
-	pb.RegisterShippingServiceServer(s, &service{repo})
+	// 向gRPC服务器注册我们的服务，这将把我们的实现绑定到protobuf定义的自动生成的接口代码中。
+	pb.RegisterShippingServiceServer(s, &service{repo: repo})
 
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
